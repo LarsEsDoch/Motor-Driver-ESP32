573ee54878841100a4cfe89ffee82c1d8536cf01
Lars <lars.bogatz@gmail.com>
2026-04-23 22:51:31 +0200

    Add WebSocket event handler
    Introduce a named onEvent callback for the AsyncWebSocket to handle WS_EVT_CONNECT, WS_EVT_DISCONNECT and WS_EVT_DATA events. The handler logs client connect/disconnect info and incoming text messages (null-terminates the payload and converts to String). Replace the previous no-op lambda with ws.onEvent(onEvent) and register the handler with the server.

--- src/main.cpp
+++ src/main.cpp
@@ -131,6 +131,30 @@ void IRAM_ATTR pulseISR() {
     }
 }
 
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+    switch (type) {
+        case WS_EVT_CONNECT:
+            Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+            break;
+        case WS_EVT_DISCONNECT:
+            Serial.printf("WebSocket client #%u disconnected\n", client->id());
+            break;
+        case WS_EVT_DATA: {
+            AwsFrameInfo *info = (AwsFrameInfo*)arg;
+            if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+                data[len] = 0;
+                String message = (char*)data;
+
+                Serial.printf("Message received: %s\n", message.c_str());
+
+            break;
+        }
+        case WS_EVT_PONG:
+        case WS_EVT_ERROR:
+            break;
+    }
+}
+
 void setup() {
     Serial.begin(921600);
     delay(2000);
@@ -171,7 +195,7 @@ void setup() {
     while (WiFiClass::status() != WL_CONNECTED) { delay(500); Serial.print("."); }
     Serial.println(WiFi.localIP());
 
-    ws.onEvent([](AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {});
+    ws.onEvent(onEvent);
     server.addHandler(&ws);
 
     if(!LittleFS.begin()){
